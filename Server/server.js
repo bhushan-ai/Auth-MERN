c03c38a4f59848917acf742f13df1f45b4bed6d5
bhushan-ai <bhushaningole2004@gmail.com>
2025-07-20 01:43:05 +0530

    updated the code

--- Server/server.js
+++ Server/server.js
@@ -9,12 +9,25 @@ const port = process.env.PORT || 8001;
 
 const app = express();
 
-// const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://auth-mern-bice.vercel.app",
+];
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
-
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 //api endpoints
 app.get("/", (req, res) => {
   return res.end("welcome");
